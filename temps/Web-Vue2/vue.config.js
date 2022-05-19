d8bbd8b5460bb5e4799a2b1faec759c22ac682c5
zijun <zhengzijun@iofod.com>
2022-05-19 15:21:00 +0800

    web: Upgrade dependencies

--- temps/Web-Vue2/vue.config.js
+++ temps/Web-Vue2/vue.config.js
@@ -5,7 +5,8 @@ module.exports = {
     config.resolve.alias = {
       ...config.resolve.alias,
       vue$: 'vue/dist/vue.js',
-      '~': path.resolve(__dirname, './src')
+      '~': path.resolve(__dirname, './src'),
+      '/assets': path.resolve(__dirname, './public/assets'),
     }
   },
   productionSourceMap: false,
